9944f237abaa1502649723a6fe7bbd6690a7a883
Aleksashin Andrei <happywin855@gmail.com>
2023-06-08 01:14:09 +0500

    Добавьте файлы проекта.

---
+++ DummyDB.Core/Models/Table.cs
@@ -0,0 +1,15 @@
+﻿namespace DummyDB.Core.Models;
+
+public class Table
+{
+    public string Name { get; private set; }
+    public List<Row> Rows { get; }
+    public Schema Schema { get; }
+
+    public Table(string name, Schema schema)
+    {
+        Name = name;
+        Rows = new List<Row>();
+        Schema = schema;
+    }
+}
